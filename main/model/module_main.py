8ca26777aa6d4764a9899e44474f6d2f6036e328
HuangYin <1071324110@qq.com>
2025-12-24 16:54:36 +0800

    Debug

--- main/model/module_main.py
+++ main/model/module_main.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import util
 
 
-class Refined_Local_Module(nn.Module):
+class Empty_Module(nn.Module):
     def __init__(self):
-        super(Refined_Local_Module, self).__init__()
+        super(Empty_Module, self).__init__()
 
     def forward(self, x):
         return
