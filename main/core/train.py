ea4205c12753ec680492c8d97d2f8d63958cc0dc
HuangYin <1071324110@qq.com>
2025-12-24 22:10:20 +0800

    Debug

--- main/core/train.py
+++ main/core/train.py
@@ -19,7 +19,7 @@ def train(config, reid_net, train_loader, criterion, optimizer, scheduler, devic
             global_feat = reid_net.global_pool(backbone_feat_map).view(B, reid_net.GLOBAL_DIM)
             global_bn_feat, global_cls_score = reid_net.global_classifier(global_feat)
             global_id_loss = criterion.ce_ls(global_cls_score, pid)
-            global_tri_loss = criterion.tri(global_feat, pid)[0]
+            global_tri_loss = criterion.tri(global_feat, pid)
             global_loss = global_id_loss + global_tri_loss
             total_loss += global_loss
             meter.update({"global_loss": global_loss.item()})
