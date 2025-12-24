653509517c2fd5304b6c02736e5a1b0fbd1ac3d8
HuangYin <1071324110@qq.com>
2025-12-24 15:59:25 +0800

    Debug

--- main/model/model.py
+++ main/model/model.py
@@ -1,12 +1,12 @@
 import copy
 
-import torch
 import torch.nn as nn
 import util
 
 from .gem_pool import GeneralizedMeanPoolingP
 from .resnet import resnet50
 from .resnet_ibn_a import resnet50_ibn_a
+from .weights_init import weights_init_classifier, weights_init_kaiming
 
 
 class ReID_Net(nn.Module):
@@ -24,10 +24,6 @@ class ReID_Net(nn.Module):
         self.GLOBAL_DIM = 2048
         self.global_pool = GeneralizedMeanPoolingP()
         self.global_classifier = Classifier(self.GLOBAL_DIM, n_class)
-
-        # self.GLOBAL_DIM = 2048 * 2
-        # self.global_pool = MaxAvgPool2d()
-        # self.global_classifier = Classifier(self.GLOBAL_DIM, n_class)
 
     def heatmap(self, img):
         B, C, H, W = img.shape
@@ -53,27 +49,6 @@ class ReID_Net(nn.Module):
 
 
 #############################################################
-class Normalize(nn.Module):
-    def __init__(self, power=2):
-        super(Normalize, self).__init__()
-        self.power = power
-
-    def forward(self, x):
-        norm = x.pow(self.power).sum(1, keepdim=True).pow(1.0 / self.power)
-        out = x.div(norm)
-        return out
-
-
-class MaxAvgPool2d(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.maxpooling = nn.AdaptiveMaxPool2d(1)
-        self.avgpooling = nn.AdaptiveAvgPool2d(1)
-
-    def forward(self, x):
-        max_f = self.maxpooling(x)
-        avg_f = self.avgpooling(x)
-        return torch.cat((max_f, avg_f), 1)
 
 
 class Classifier(nn.Module):
@@ -132,82 +107,3 @@ class Backbone(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         return out
-
-
-#############################################################
-class Non_local(nn.Module):
-    def __init__(self, in_channels, reduc_ratio=2):
-        super(Non_local, self).__init__()
-
-        self.in_channels = in_channels
-        self.inter_channels = reduc_ratio // reduc_ratio
-
-        self.g = nn.Sequential(
-            nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1, stride=1, padding=0),
-        )
-
-        self.W = nn.Sequential(
-            nn.Conv2d(in_channels=self.inter_channels, out_channels=self.in_channels, kernel_size=1, stride=1, padding=0),
-            nn.BatchNorm2d(self.in_channels),
-        )
-        nn.init.constant_(self.W[1].weight, 0.0)
-        nn.init.constant_(self.W[1].bias, 0.0)
-
-        self.theta = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1, stride=1, padding=0)
-
-        self.phi = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1, stride=1, padding=0)
-
-    def forward(self, x):
-        """
-        :param x: (b, c, t, h, w)
-        :return:
-        """
-
-        batch_size = x.size(0)
-        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
-        g_x = g_x.permute(0, 2, 1)
-
-        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
-        theta_x = theta_x.permute(0, 2, 1)
-        phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
-        f = torch.matmul(theta_x, phi_x)
-        N = f.size(-1)
-        # f_div_C = torch.nn.functional.softmax(f, dim=-1)
-        f_div_C = f / N
-
-        y = torch.matmul(f_div_C, g_x)
-        y = y.permute(0, 2, 1).contiguous()
-        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
-        W_y = self.W(y)
-        z = W_y + x
-
-        return z
-
-
-#############################################################
-def weights_init_classifier(m):
-    classname = m.__class__.__name__
-    if classname.find("Linear") != -1:
-        nn.init.normal_(m.weight, std=0.001)
-        if m.bias:
-            nn.init.constant_(m.bias, 0.0)
-
-
-def weights_init_kaiming(m):
-    classname = m.__class__.__name__
-    if classname.find("Linear") != -1:
-        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_out")
-        if m.bias is not None:
-            nn.init.constant_(m.bias, 0)
-    elif classname.find("Conv") != -1:
-        nn.init.kaiming_normal_(m.weight, a=0, mode="fan_in")
-        if m.bias is not None:
-            nn.init.constant_(m.bias, 0.0)
-    elif classname.find("BatchNorm") != -1:
-        if m.affine:
-            nn.init.constant_(m.weight, 1.0)
-            nn.init.constant_(m.bias, 0.0)
-    elif classname.find("InstanceNorm") != -1:
-        if m.affine:
-            nn.init.constant_(m.weight, 1.0)
-            nn.init.constant_(m.bias, 0.0)
---
+++ main/model/module_main.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import util
+
+
+class Refined_Local_Module(nn.Module):
+    def __init__(self):
+        super(Refined_Local_Module, self).__init__()
+
+    def forward(self, x):
+        return
