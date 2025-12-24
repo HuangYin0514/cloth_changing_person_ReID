15748c17740ffe2e75cc84fdb11a8f1192f4dbcf
HuangYin <1071324110@qq.com>
2025-12-24 16:25:39 +0800

    Debug

--- main/loss/triplet_loss.py
+++ main/loss/triplet_loss.py
@@ -1,6 +1,16 @@
 import torch
-import torch.nn.functional as F
 from torch import nn
+
+
+def normalize(x, axis=-1):
+    """Normalizing to unit length along the specified dimension.
+    Args:
+      x: pytorch Variable
+    Returns:
+      x: pytorch Variable, same shape as input
+    """
+    x = 1.0 * x / (torch.norm(x, 2, axis, keepdim=True).expand_as(x) + 1e-12)
+    return x
 
 
 def euclidean_dist(x, y):
@@ -15,12 +25,30 @@ def euclidean_dist(x, y):
     xx = torch.pow(x, 2).sum(1, keepdim=True).expand(m, n)
     yy = torch.pow(y, 2).sum(1, keepdim=True).expand(n, m).t()
     dist = xx + yy
-    dist.addmm_(x, y.t(), beta=1, alpha=-2)
+    dist = dist - 2 * torch.matmul(x, y.t())
+    # dist.addmm_(1, -2, x, y.t())
     dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
     return dist
 
 
-def hard_example_mining(dist_mat, labels, mining_method="batch_hard", return_inds=False):
+def cosine_dist(x, y):
+    """
+    Args:
+      x: pytorch Variable, with shape [m, d]
+      y: pytorch Variable, with shape [n, d]
+    Returns:
+      dist: pytorch Variable, with shape [m, n]
+    """
+    m, n = x.size(0), y.size(0)
+    x_norm = torch.pow(x, 2).sum(1, keepdim=True).sqrt().expand(m, n)
+    y_norm = torch.pow(y, 2).sum(1, keepdim=True).sqrt().expand(n, m).t()
+    xy_intersection = torch.mm(x, y.t())
+    dist = xy_intersection / (x_norm * y_norm)
+    dist = (1.0 - dist) / 2
+    return dist
+
+
+def hard_example_mining(dist_mat, labels, return_inds=False):
     """For each anchor, find the hardest positive and negative sample.
     Args:
       dist_mat: pytorch Variable, pair wise distance between samples, shape [N, N]
@@ -45,33 +73,14 @@ def hard_example_mining(dist_mat, labels, mining_method="batch_hard", return_ind
     is_pos = labels.expand(N, N).eq(labels.expand(N, N).t())
     is_neg = labels.expand(N, N).ne(labels.expand(N, N).t())
 
-    if mining_method == "batch_hard":
-        # `dist_ap` means distance(anchor, positive)
-        # both `dist_ap` and `relative_p_inds` with shape [N, 1]
-        dist_ap, relative_p_inds = torch.max(dist_mat[is_pos].contiguous().view(N, -1), 1, keepdim=True)
-        # `dist_an` means distance(anchor, negative)
-        # both `dist_an` and `relative_n_inds` with shape [N, 1]
-        dist_an, relative_n_inds = torch.min(dist_mat[is_neg].contiguous().view(N, -1), 1, keepdim=True)
-        # shape [N]
-    elif mining_method == "batch_sample":
-        dist_mat_ap = dist_mat[is_pos].contiguous().view(N, -1)
-        relative_p_inds = torch.multinomial(F.softmax(dist_mat_ap, dim=1), num_samples=1)
-        dist_ap = torch.gather(dist_mat_ap, 1, relative_p_inds)
-
-        dist_mat_an = dist_mat[is_neg].contiguous().view(N, -1)
-        relative_n_inds = torch.multinomial(F.softmin(dist_mat_an, dim=1), num_samples=1)
-        dist_an = torch.gather(dist_mat_an, 1, relative_n_inds)
-    elif mining_method == "batch_soft":
-        dist_mat_ap = dist_mat[is_pos].contiguous().view(N, -1)
-        dist_mat_an = dist_mat[is_neg].contiguous().view(N, -1)
-        weight_ap = torch.exp(dist_mat_ap) / torch.exp(dist_mat_ap).sum(dim=1, keepdim=True)
-        weight_an = torch.exp(-dist_mat_an) / torch.exp(-dist_mat_an).sum(dim=1, keepdim=True)
-
-        dist_ap = (weight_ap * dist_mat_ap).sum(dim=1, keepdim=True)
-        dist_an = (weight_an * dist_mat_an).sum(dim=1, keepdim=True)
-    else:
-        print("error, unsupported mining method {}".format(mining_method))
-
+    # `dist_ap` means distance(anchor, positive)
+    # both `dist_ap` and `relative_p_inds` with shape [N, 1]
+    dist_ap, relative_p_inds = torch.max(dist_mat[is_pos].contiguous().view(N, -1), 1, keepdim=True)
+    # print(dist_mat[is_pos].shape)
+    # `dist_an` means distance(anchor, negative)
+    # both `dist_an` and `relative_n_inds` with shape [N, 1]
+    dist_an, relative_n_inds = torch.min(dist_mat[is_neg].contiguous().view(N, -1), 1, keepdim=True)
+    # shape [N]
     dist_ap = dist_ap.squeeze(1)
     dist_an = dist_an.squeeze(1)
 
@@ -90,25 +99,31 @@ def hard_example_mining(dist_mat, labels, mining_method="batch_hard", return_ind
 
 
 class TripletLoss(object):
-    """Modified from Tong Xiao's open-reid (https://github.com/Cysu/open-reid).
-    Related Triplet Loss theory can be found in paper 'In Defense of the Triplet
-    Loss for Person Re-Identification'."""
+    """
+    Triplet loss using HARDER example mining,
+    modified based on original triplet loss using hard example mining
+    """
 
-    def __init__(self, margin=None, mining_method="batch_hard"):
+    def __init__(self, margin=None, hard_factor=0.0):
         self.margin = margin
-        self.mining_method = mining_method
+        self.hard_factor = hard_factor
         if margin is not None:
             self.ranking_loss = nn.MarginRankingLoss(margin=margin)
         else:
             self.ranking_loss = nn.SoftMarginLoss()
 
-    def __call__(self, features, labels):
-        dist_mat = euclidean_dist(features, features)
-        dist_ap, dist_an = hard_example_mining(dist_mat, labels, self.mining_method)
+    def __call__(self, global_feat, labels, normalize_feature=False):
+        if normalize_feature:
+            global_feat = normalize(global_feat, axis=-1)
+        dist_mat = euclidean_dist(global_feat, global_feat)
+        dist_ap, dist_an = hard_example_mining(dist_mat, labels)
+
+        dist_ap *= 1.0 + self.hard_factor
+        dist_an *= 1.0 - self.hard_factor
 
         y = dist_an.new().resize_as_(dist_an).fill_(1)
         if self.margin is not None:
             loss = self.ranking_loss(dist_an, dist_ap, y)
         else:
             loss = self.ranking_loss(dist_an - dist_ap, y)
-        return loss
+        return loss, dist_ap, dist_an
