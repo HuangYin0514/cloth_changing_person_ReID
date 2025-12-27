7a29cfce4a9898912147d12c3d2a674de0bf73cd
HuangYin <1071324110@qq.com>
2025-12-28 04:15:24 +0800

    Debug

--- main/core/train.py
+++ main/core/train.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, scheduler, device, epoch, logger):
     scheduler.step(epoch)
     reid_net.train()
+    clothe_base.clothe_classifier.train()
     meter = util.MultiItemAverageMeter()
     for epoch, data in enumerate(tqdm(train_loader)):
         img, pid, camid, clotheid = data
@@ -18,16 +19,6 @@ def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, sch
 
             global_feat = reid_net.global_pool(backbone_feat_map).view(B, reid_net.GLOBAL_DIM)
             global_bn_feat, global_cls_score = reid_net.global_classifier(global_feat)
-
-            if epoch > 25:
-                # 提升衣服分类器性能
-                clothe_base.clothe_classifier.train()
-                clothe_cls_score = clothe_base.clothe_classifier(global_bn_feat.detach())
-                clothe_loss = clothe_base.criterion_cfl(clothe_cls_score, clotheid)
-                clothe_base.optimizer.zero_grad()
-                clothe_loss.backward()
-                clothe_base.optimizer.step()
-
             global_id_loss = criterion.ce_ls(global_cls_score, pid)
             global_tri_loss = criterion.tri(global_feat, pid)
             global_loss = global_id_loss + global_tri_loss
@@ -35,8 +26,15 @@ def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, sch
             total_loss += global_loss
 
             if epoch > 25:
+                # 提升衣服分类器性能
+                clothe_cls_score = clothe_base.clothe_classifier(global_feat.detach())
+                clothe_loss = clothe_base.criterion_cfl(clothe_cls_score, clotheid)
+                clothe_base.optimizer.zero_grad()
+                clothe_loss.backward()
+                clothe_base.optimizer.step()
+
                 # 混淆骨干网络衣服信息
-                new_clothe_cls_score = clothe_base.clothe_classifier(global_bn_feat)
+                new_clothe_cls_score = clothe_base.clothe_classifier(global_feat)
                 global_clothe_adv_loss = clothe_base.criterion_adv(new_clothe_cls_score, clotheid, clothe_base.pid2clothes[pid])
                 total_loss += global_clothe_adv_loss
 
