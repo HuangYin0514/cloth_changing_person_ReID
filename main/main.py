b086b20cf0508c970345d707905654b2989495e6
HuangYin <1071324110@qq.com>
2025-12-27 22:21:21 +0800

    Debug

--- main/main.py
+++ main/main.py
@@ -5,7 +5,6 @@ import warnings
 
 import torch
 import util
-from build_clothes_base import Build_Clothe_BASE
 from build_criterion import Build_Criterion
 from build_optimizer import Build_Optimizer
 from build_scheduler import Build_Scheduler
@@ -49,7 +48,6 @@ def run(config):
     # Model
     reid_net = ReID_Net(config, dataset.num_train_pids).to(device)
     # logger("Model:\n {}".format(reid_net))
-    clothe_base = Build_Clothe_BASE(config, reid_net.GLOBAL_DIM, dataset.num_train_clothes, dataset.pid2clothes, device)
 
     ######################################################################
     # Criterion
@@ -71,7 +69,7 @@ def run(config):
     # 初始化最佳指标
     best_epoch, best_mAP, best_rank1 = 0, 0, 0
     for epoch in range(0, config.OPTIMIZER.TOTAL_TRAIN_EPOCH):
-        meter = train(config, reid_net, clothe_base, train_loader, criterion, optimizer, scheduler, device, epoch, logger)
+        meter = train(config, reid_net, train_loader, criterion, optimizer, scheduler, device, epoch, logger)
         logger("Time: {}; Epoch: {}; {}".format(util.time_now(), epoch, meter.get_str()))
         wandb.log({"Lr": optimizer.param_groups[0]["lr"], **meter.get_dict()})
 
