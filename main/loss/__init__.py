3adaedea53318e8271c48bd76b67938a7f25a962
HuangYin <1071324110@qq.com>
2025-12-28 09:58:59 +0800

    Debug

--- main/loss/__init__.py
+++ main/loss/__init__.py
@@ -1,4 +1,2 @@
 from .ce_label_smooth import CrossEntropyLabelSmooth
-from .clothes_adversarial_loss import ClothesBasedAdversarialLoss
-from .cosface_loss import CosFaceLoss
 from .triplet_loss import TripletLoss
