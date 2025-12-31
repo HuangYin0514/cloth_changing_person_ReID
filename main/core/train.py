29433a82aa95f1d22a6f7ecd3d8f991274733ae3
HuangYin <1071324110@qq.com>
2025-12-31 17:45:57 +0800

    055

--- main/core/train.py
+++ main/core/train.py
@@ -33,12 +33,6 @@ def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, sch
             meter.update({"global_loss": global_loss.item()})
             total_loss += global_loss
 
-            if epoch > 25:
-                # 混淆骨干网络衣服信息
-                new_clothe_cls_score = clothe_base.clothe_classifier(global_bn_feat)
-                global_clothe_adv_loss = clothe_base.criterion_adv(new_clothe_cls_score, clotheid, clothe_base.pid2clothes[pid])
-                total_loss += global_clothe_adv_loss
-
             optimizer.zero_grad()
             total_loss.backward()
             optimizer.step()
