e62ade1c8b5d8ad05b0b38a1e8b289a79868b7e1
HuangYin <1071324110@qq.com>
2025-12-31 17:43:17 +0800

    054

--- main/core/train.py
+++ main/core/train.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, scheduler, device, epoch, logger):
     scheduler.step(epoch)
     reid_net.train()
+    clothe_base.clothe_classifier.train()
     meter = util.MultiItemAverageMeter()
     for epoch, data in enumerate(tqdm(train_loader)):
         img, pid, camid, clotheid = data
@@ -16,6 +17,14 @@ def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, sch
 
             backbone_feat_map, global_feat, global_bn_feat = reid_net(img)
 
+            if epoch > 25:
+                # 提升衣服分类器性能
+                clothe_cls_score = clothe_base.clothe_classifier(global_bn_feat.detach())
+                clothe_loss = clothe_base.criterion_cfl(clothe_cls_score, clotheid)
+                clothe_base.optimizer.zero_grad()
+                clothe_loss.backward()
+                clothe_base.optimizer.step()
+
             # Global
             global_cls_score = reid_net.global_classifier(global_bn_feat)
             global_id_loss = criterion.ce_ls(global_cls_score, pid)
@@ -25,14 +34,6 @@ def train(config, reid_net, clothe_base, train_loader, criterion, optimizer, sch
             total_loss += global_loss
 
             if epoch > 25:
-                # 提升衣服分类器性能
-                clothe_base.clothe_classifier.train()
-                clothe_cls_score = clothe_base.clothe_classifier(global_bn_feat.detach())
-                clothe_loss = clothe_base.criterion_cfl(clothe_cls_score, clotheid)
-                clothe_base.optimizer.zero_grad()
-                clothe_loss.backward()
-                clothe_base.optimizer.step()
-
                 # 混淆骨干网络衣服信息
                 new_clothe_cls_score = clothe_base.clothe_classifier(global_bn_feat)
                 global_clothe_adv_loss = clothe_base.criterion_adv(new_clothe_cls_score, clotheid, clothe_base.pid2clothes[pid])
