e946ca1c4502567d4d1e34df190ffe13f7777240
HuangYin <1071324110@qq.com>
2026-01-09 21:16:48 +0800

    99

--- main/util/set_seed_torch.py
+++ main/util/set_seed_torch.py
@@ -3,16 +3,26 @@ import random
 
 import numpy as np
 import torch
+import torch.backends.cudnn as cudnn
+
+# def set_seed_torch(seed):
+#     seed = int(seed)
+#     random.seed(seed)
+#     os.environ["PYTHONASHSEED"] = str(seed)
+#     np.random.seed(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed(seed)
+#     torch.cuda.manual_seed_all(seed)
+#     # 程序浮现 torch.backends.cudnn.benchmark = False / torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.benchmark = False
+#     torch.backends.cudnn.deterministic = True
 
 
 def set_seed_torch(seed):
-    seed = int(seed)
     random.seed(seed)
-    os.environ["PYTHONASHSEED"] = str(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    # 程序浮现 torch.backends.cudnn.benchmark = False / torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
+    cudnn.deterministic = True
+    cudnn.benchmark = False
