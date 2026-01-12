508f4f33c99f86c0c34051f9efb04d02482eb9b3
HuangYin <1071324110@qq.com>
2026-01-12 17:38:53 +0800

    113

--- main/core/train.py
+++ main/core/train.py
@@ -20,38 +20,36 @@ def train(config, reid_net, train_loader, criterion, optimizer, scheduler, devic
             global_id_loss = criterion.ce_ls(global_cls_score, pid)
             meter.update({"global_id_loss": global_id_loss.item()})
             total_loss += global_id_loss
-            if epoch > 35:
-                global_tri_loss = criterion.tri(global_feat, pid)
-                meter.update({"global_tri_loss": global_tri_loss.item()})
-                total_loss += global_tri_loss
+            global_tri_loss = criterion.tri(global_feat, pid)
+            meter.update({"global_tri_loss": global_tri_loss.item()})
+            total_loss += global_tri_loss
 
-            # 定位
-            clothe_cls_score = clothe_base.clothe_classifier_net(backbone_feat_map.detach())
-            clothe_loss = clothe_base.criterion_ce(clothe_cls_score, clotheid)
-            meter.update({"clothe_loss": clothe_loss.item()})
-            clothe_base.optimizer.zero_grad()
-            clothe_loss.backward()
-            clothe_base.optimizer.step()
-            clothe_feat_map = reid_net.clothe_cam_position(backbone_feat_map, clotheid, clothe_base.clothe_classifier_net)
-            id_feat_map = reid_net.clothe_cam_position(backbone_feat_map, pid, reid_net.global_classifier)
-
-            # 去除衣服
-            unclothe_cam_feat_map = id_feat_map - clothe_feat_map
-            unclothe_cam_feat = reid_net.clothe_cam_pool(unclothe_cam_feat_map).view(B, reid_net.GLOBAL_DIM)
-            unclothe_cam_feat_bn_feat = reid_net.clothe_cam_bn_neck(unclothe_cam_feat)
-            unclothe_cam_cls_score = reid_net.clothe_cam_classifier(unclothe_cam_feat_bn_feat)
-            unclothe_cam_id_loss = criterion.ce_ls(unclothe_cam_cls_score, pid)
-            meter.update({"unclothe_cam_id_loss": unclothe_cam_id_loss.item()})
-            total_loss += unclothe_cam_id_loss
             if epoch > 35:
+                # 定位
+                clothe_cls_score = clothe_base.clothe_classifier_net(backbone_feat_map.detach())
+                clothe_loss = clothe_base.criterion_ce(clothe_cls_score, clotheid)
+                meter.update({"clothe_loss": clothe_loss.item()})
+                clothe_base.optimizer.zero_grad()
+                clothe_loss.backward()
+                clothe_base.optimizer.step()
+                clothe_feat_map = reid_net.clothe_cam_position(backbone_feat_map, clotheid, clothe_base.clothe_classifier_net)
+
+                # 去除衣服
+                unclothe_cam_feat_map = backbone_feat_map - clothe_feat_map
+                unclothe_cam_feat = reid_net.clothe_cam_pool(unclothe_cam_feat_map).view(B, reid_net.GLOBAL_DIM)
+                unclothe_cam_feat_bn_feat = reid_net.clothe_cam_bn_neck(unclothe_cam_feat)
+                unclothe_cam_cls_score = reid_net.clothe_cam_classifier(unclothe_cam_feat_bn_feat)
+                unclothe_cam_id_loss = criterion.ce_ls(unclothe_cam_cls_score, pid)
+                meter.update({"unclothe_cam_id_loss": unclothe_cam_id_loss.item()})
+                total_loss += unclothe_cam_id_loss
                 unclothe_cam_tri_loss = criterion.tri(unclothe_cam_feat, pid)
                 meter.update({"unclothe_cam_tri_loss": unclothe_cam_tri_loss.item()})
                 total_loss += unclothe_cam_tri_loss
 
-            # 蒸馏
-            propagation_loss = 0.1 * reid_net.propagation(student_logits=global_cls_score, teacher_logits=unclothe_cam_cls_score)
-            meter.update({"propagation_loss": propagation_loss.item()})
-            total_loss += propagation_loss
+                # 蒸馏
+                propagation_loss = 0.1 * reid_net.propagation(student_logits=global_cls_score, teacher_logits=unclothe_cam_cls_score)
+                meter.update({"propagation_loss": propagation_loss.item()})
+                total_loss += propagation_loss
 
         optimizer.zero_grad()
         total_loss.backward()
