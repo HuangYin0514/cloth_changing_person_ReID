c5fa101bd4b13903ec39700bac2de659fd810a2a
HuangYin <1071324110@qq.com>
2026-01-17 22:03:02 +0800

    feat: 162-B

--- main/model/model.py
+++ main/model/model.py
@@ -5,7 +5,6 @@ from .bn_neck import BN_Neck
 from .cam import CAM
 from .classifier import Linear_Classifier
 from .gem_pool import GeneralizedMeanPoolingP
-from .pool_attention import Pool_Attention
 from .resnet import resnet50
 from .resnet_ibn_a import resnet50_ibn_a
 
