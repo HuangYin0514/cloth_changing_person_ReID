76ee2cbd80263244b1a52a060068c491ae9049b4
HuangYin <1071324110@qq.com>
2026-01-21 19:38:40 +0800

    feat: 192-B_Cam_KD_Cor_Msi

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # cloth_changing_person_ReID
+
+
+os block https://github.com/MatthewAbugeja/osnet/blob/master/torchreid/models/osnet.py
+https://arxiv.org/pdf/1910.06827v5
+
+189 三个并行结构进行判别
+0.17963
+0.42602
+
+190 Multi-granularity Cross Transformer Network for person re-identification
+0.17243
+0.375
+
+191  return F.relu(out) + identity
+0.17184
+0.37755
+
+192 修改att
+https://github.com/xmu-xiaoma666/External-Attention-pytorch/blob/master/model/attention/CBAM.py
--- main/model/osblock.py
+++ main/model/osblock.py
@@ -6,16 +6,6 @@ from torch.nn import functional as F
 os block https://github.com/MatthewAbugeja/osnet/blob/master/torchreid/models/osnet.py
 https://arxiv.org/pdf/1910.06827v5
 
-189 三个并行结构进行判别
-0.17963
-0.42602
-
-190 Multi-granularity Cross Transformer Network for person re-identification
-0.17243
-0.375
-
-191  return F.relu(out) + identity
-
 """
 
 
@@ -70,43 +60,21 @@ class LightConv3x3(nn.Module):
         return x
 
 
-class ChannelGate(nn.Module):
-    """A mini-network that generates channel-wise gates conditioned on input tensor."""
-
-    def __init__(self, in_channels, num_gates=None, return_gates=False, gate_activation="sigmoid", reduction=16, layer_norm=False):
-        super(ChannelGate, self).__init__()
-        if num_gates is None:
-            num_gates = in_channels
-        self.return_gates = return_gates
-        self.global_avgpool = nn.AdaptiveAvgPool2d(1)
-        self.fc1 = nn.Conv2d(in_channels, in_channels // reduction, kernel_size=1, bias=True, padding=0)
-        self.norm1 = None
-        if layer_norm:
-            self.norm1 = nn.LayerNorm((in_channels // reduction, 1, 1))
-        self.relu = nn.ReLU(inplace=True)
-        self.fc2 = nn.Conv2d(in_channels // reduction, num_gates, kernel_size=1, bias=True, padding=0)
-        if gate_activation == "sigmoid":
-            self.gate_activation = nn.Sigmoid()
-        elif gate_activation == "relu":
-            self.gate_activation = nn.ReLU(inplace=True)
-        elif gate_activation == "linear":
-            self.gate_activation = None
-        else:
-            raise RuntimeError("Unknown gate activation: {}".format(gate_activation))
+class ChannelAttention(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super().__init__()
+        self.maxpool = nn.AdaptiveMaxPool2d(1)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.se = nn.Sequential(nn.Conv2d(channel, channel // reduction, 1, bias=False), nn.ReLU(), nn.Conv2d(channel // reduction, channel, 1, bias=False))
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        input = x
-        x = self.global_avgpool(x)
-        x = self.fc1(x)
-        if self.norm1 is not None:
-            x = self.norm1(x)
-        x = self.relu(x)
-        x = self.fc2(x)
-        if self.gate_activation is not None:
-            x = self.gate_activation(x)
-        if self.return_gates:
-            return x
-        return input * x
+        max_result = self.maxpool(x)
+        avg_result = self.avgpool(x)
+        max_out = self.se(max_result)
+        avg_out = self.se(avg_result)
+        output = self.sigmoid(max_out + avg_out)
+        return output
 
 
 class OSBlock(nn.Module):
@@ -115,7 +83,7 @@ class OSBlock(nn.Module):
     def __init__(self, in_channels, out_channels, bottleneck_reduction=4, **kwargs):
         super(OSBlock, self).__init__()
         mid_channels = out_channels // bottleneck_reduction
-        self.conv_in = Conv1x1(in_channels, mid_channels)
+        self.conv1 = Conv1x1(in_channels, mid_channels)
         self.conv2a = LightConv3x3(mid_channels, mid_channels)
         self.conv2b = nn.Sequential(
             LightConv3x3(mid_channels, mid_channels),
@@ -126,19 +94,19 @@ class OSBlock(nn.Module):
             LightConv3x3(mid_channels, mid_channels),
             LightConv3x3(mid_channels, mid_channels),
         )
-        self.gate = ChannelGate(mid_channels)
-        self.conv_out = Conv1x1Linear(mid_channels, out_channels)
+        self.gate = ChannelAttention(mid_channels)
+        self.conv3 = Conv1x1Linear(mid_channels, out_channels)
 
     def forward(self, x):
         identity = x
-        x1 = self.conv_in(x)
+        x1 = self.conv1(x)
         x2a = self.conv2a(x1)
         x2b = self.conv2b(x1)
         x2c = self.conv2c(x1)
         x2 = self.gate(x2a) + self.gate(x2b) + self.gate(x2c)
-        x3 = self.conv_out(x2)
-        out = x3
-        return F.relu(out) + identity
+        x3 = self.conv3(x2)
+        out = x3 + identity
+        return F.relu(out)
 
 
 if __name__ == "__main__":
