047d704cc2cba7c3f5c34e19e415fd318970aa14
HuangYin <1071324110@qq.com>
2026-01-23 12:20:49 +0800

    feat: 204-B_Cam_KD_Cor_Msi_prccV196

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -10,5 +10,6 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 python main.py --config_file "config/method.yml" TASK.NOTES=Debug TASK.NAME=Lucky
 
 ############################################################
-# Reg_db
+# prcc
 ############################################################
+python main.py --config_file "config/method.yml" TASK.NOTES=Debug TASK.NAME=Lucky DATA.TRAIN_DATASET=prcc DATA.TRAIN_DATASET=xxxxxxx
