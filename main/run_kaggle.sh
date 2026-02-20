2f5297ae09c9c3e93eb3a0dcdffb6e6560ba3b24
HuangYin <1071324110@qq.com>
2026-02-20 22:52:49 +0800

    feat: 250-Vis_rank

--- main/run_kaggle.sh
+++ main/run_kaggle.sh
@@ -22,10 +22,12 @@ wandb login c74133df8c2cf575304acf8a99fe03ab74b6fe6a
 # python main.py --config_file "config/method.yml" TASK.NOTES=Debug TASK.NAME=Lucky MODEL.KD=0.010
 
 # =================== 可视化  ==============================
+VERSION_NAME="250-Vis_rank"
+
 # 方法可视化
 mkdir -p results/outputs/models
 gdown -O results/outputs/models/model_89.pth 1TEsdxgbhxxuQ5Jheej6kvNbjCfVp_i6Y 
-python vis_main.py --config_file "config/method.yml" TASK.NOTES=245-Vis_rank TEST.RESUME_EPOCH=89
+python vis_main.py --config_file "config/method.yml" TASK.NOTES=${VERSION_NAME} TEST.RESUME_EPOCH=89
 tar -czf ../result_method.tar.gz results
 rm -rf results/outputs/*
 
